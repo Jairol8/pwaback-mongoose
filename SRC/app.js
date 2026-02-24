5806df6da500cb79dcfa7cc658436c98d960f958
Jairo <jr1312982@gmail.com>
2026-02-23 21:17:04 -0600

    first commit

---
+++ SRC/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import morgan  from "morgan";
+import authRoutes from "./routes/auth.routes";
+import taskRoutes from "./routes/task.routes";
+import { connectToDB } from "./db/connect";
+
+const app = express();
+
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        process.env.FRONT_ORIGIN || ""
+    ].filter(Boolean),
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+
+app.use(async(_req, _res, next)=>{
+    try{
+        await connectToDB(); next();
+    }catch(e){next(e);}
+});
+
+app.get("/", (_req,res)=> res.json({ok:true,name:"Jairo Todo Api"}));
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+export default app;
---
+++ api/index.js
@@ -0,0 +1,8 @@
+//api/index.js
+import "dotenv/config";
+import app from "../SRC/app.js";
+
+
+export default app;
+
+
