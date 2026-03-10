9e838e618cf7c97a386a253f93d084e448e9c0fc
Jairo <jr1312982@gmail.com>
2026-03-09 23:43:07 -0600

    update backend PWA

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth.js";
-import { list, create, update, remove} from "../controllers/task.controller.js";  //, bulksync 
+import { list, create, update, remove, bulksync} from "../controllers/task.controller.js";  //, bulksync 
 
 const router = Router();
 
@@ -10,6 +10,6 @@ router.get('/', list); // Obtener todas las tareas
 router.post('/', create); // Crear una nueva tarea
 router.put('/:id', update); // Actualizar una tarea por ID
 router.delete('/:id', remove); // Eliminar una tarea por ID
-//router.post('/bulksync', bulksync); // Sincronización masiva de tareas
+router.post('/bulksync', bulksync); // Sincronización masiva de tareas
 
 export default router;
